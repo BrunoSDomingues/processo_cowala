89deade89681669a353525d6fbfd2ef22d7fcd5d
Bruno Domingues <brunao.signorelli@hotmail.com>
2022-01-21 17:58:52 -0300

    Commented console log

--- algoritmos/validade.js
+++ algoritmos/validade.js
@@ -7,5 +7,5 @@ function checkExpired(date, exp) {
     return myDate > today;
 }
 
-console.log(checkExpired("2021-11-17T20:40:09.503Z", "10d"));
-console.log(checkExpired("2021-12-10T00:00:00.000Z", "180d"));
+// console.log(checkExpired("2021-11-17T20:40:09.503Z", "10d"));
+// console.log(checkExpired("2021-12-10T00:00:00.000Z", "180d"));
