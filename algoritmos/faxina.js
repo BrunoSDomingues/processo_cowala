4789d6fa2bff63bab091b3052f927b9884f382c8
Bruno Domingues <brunao.signorelli@hotmail.com>
2022-01-21 16:47:58 -0300

    Faxina done

---
+++ algoritmos/faxina.js
@@ -0,0 +1,9 @@
+function removeEmpty(obj) {
+    Object.keys(obj).forEach(key => {
+        if (obj[key] === null || obj[key] === undefined) delete obj[key];
+    });
+
+    return obj;
+}
+
+// console.log(removeEmpty({"fizz": "buzz", "foo": null, "bar": 42}))
